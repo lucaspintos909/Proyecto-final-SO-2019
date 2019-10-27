d4cc00ae6446b6f05e4a58ba686ec4a60f23e0b8
Gaizka <gaizkagomez97@gmail.com>
2019-10-27 15:43:41 -0300

    pruebiña

--- programas/compilacion.bat
+++ programas/compilacion.bat
@@ -2,7 +2,7 @@
 goto inicio
 
 :inicio
-set path=%path%C:\Program Files\Java\jdk1.8.0_221\bin;
+set path=%path%C:\Program Files\Java\jdk1.7.0_79\bin;
 echo   /---------PLATON--------\
 echo .
 echo /--------COMPILACION---------\
