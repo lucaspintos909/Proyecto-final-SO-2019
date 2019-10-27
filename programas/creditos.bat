b7a47478e631be16bc8f609d14d44a5b450310a9
Gaizka <gaizkagomez97@gmail.com>
2019-10-27 18:02:32 -0300

    creditos

---
+++ programas/creditos.bat
@@ -0,0 +1,47 @@
+echo off
+
+echo                                                                                                     / PLATON \
+echo                                                                                                    /----------\
+echo                                                                                           /----------CREDITOS----------\
+echo .
+echo                                                                                                   _-INTEGRANTES-_
+echo .
+echo                                                                                                   /            \
+echo                                                                                                    Lucas Pintos
+echo                                                                                                   \            /
+echo .
+echo                                                                                                   /            \
+echo                                                                                                    Gaizka Gomez
+echo                                                                                                   \            /
+echo .
+echo                                                                                                   -_INTEGRANTES_-
+echo .
+echo .
+echo                                                                                                  CENTRO Y PROFESOR
+echo .
+echo                                                                                                        /   \
+echo                                                                                                         ESI 
+echo                                                                                                        \   /
+echo .
+echo                                                                                                  /              \
+echo                                                                                                   Fabian Cabrera
+echo                                                                                                  \              /
+echo .
+echo                                                                                                  CENTRO Y PROFESOR
+echo .
+echo .
+echo                                                                                                  CLASE Y HORARIOS
+echo .
+echo                                                                                                    /          \
+echo                                                                                                     Clase 1_BC
+echo                                                                                                    \          /
+echo .
+echo                                                                                                  /              \
+echo                                                                                                   Turno Matutino
+echo                                                                                                  \              /
+echo .
+echo                                                                                                  CLASE Y HORARIOS
+echo .
+echo                                                                                           \----------CREDITOS----------/
+echo                                                                                                    \----------/
+echo                                                                                                     \ PLATON /
