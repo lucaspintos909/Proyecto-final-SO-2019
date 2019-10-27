fb7bf74f691bc66c52a6edca79332a85e34fba63
Gaizka <gaizkagomez97@gmail.com>
2019-10-27 13:57:18 -0300

    prueba

--- programas/ejecucion.bat
+++ programas/ejecucion.bat
@@ -3,10 +3,15 @@ goto inicio
 
 
 :inicio
-echo ----------EJECUCION----------
+cls
+echo   /---------PLATON--------\
+echo 
+echo /----------EJECUCION---------\
 echo 1) Ejecutar proyecto
 echo 0) VOLVER
-echo ----------EJECUCION----------
+echo \----------EJECUCION---------/
+echo 
+echo   \---------PLATON--------/
 set/p opcion=- Opcion: 
 if %opcion% EQU 1 = goto ejecutar
 if %opcion% EQU 0 = call IDE
@@ -20,7 +25,7 @@ set /p paquete=-Escriba el nombre del paquete del proyecto:
 set /p clase=-Escriba el nombre del programa que quiera ejecutar:
 if EXIST workspace\%nombre%\obj\%paquete% (
 cd workspace\%nombre%\obj\%paquete%
-set CLASSPATH=%CLASSPATH%.;
+set CLASSPATH=%CLASSPATH%.;workspace\%nombre%\obj\%paquete%
 del workspace\%nombre%\obj\%paquete%\%clase%.java
 java workspace\%nombre%\obj\%paquete%\%clase%
 echo La ejecucion del programa se ha completado) else (echo El nombre del proyecto ingresado no existe!)
