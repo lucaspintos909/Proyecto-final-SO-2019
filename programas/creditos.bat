43e9252dd42d280448d29bc7ec36a24e537185e2
Gaizka <gaizkagomez97@gmail.com>
2019-10-27 19:39:50 -0300

    creditos mejorado

--- programas/creditos.bat
+++ programas/creditos.bat
@@ -1,49 +1,49 @@
 @echo off
 
-echo                                                                                                     / PLATON \
-echo                                                                                                    /----------\
-echo                                                                                           /----------CREDITOS----------\
+echo                                                                                                                           / PLATON \
+echo                                                                                                                          /----------\
+echo                                                                                                                 /----------CREDITOS----------\
 echo .
-echo                                                                                                   _-INTEGRANTES-_
+echo                                                                                                                         _-INTEGRANTES-_
 echo .
-echo                                                                                                   /            \
-echo                                                                                                    Lucas Pintos
-echo                                                                                                   \            /
+echo                                                                                                                         /            \
+echo                                                                                                                          Lucas Pintos
+echo                                                                                                                         \            /
 echo .
-echo                                                                                                   /            \
-echo                                                                                                    Gaizka Gomez
-echo                                                                                                   \            /
+echo                                                                                                                         /            \
+echo                                                                                                                          Gaizka Gomez
+echo                                                                                                                         \            /
 echo .
-echo                                                                                                   -_INTEGRANTES_-
-echo .
-echo .
-echo                                                                                                  CENTRO Y PROFESOR
-echo .
-echo                                                                                                        /   \
-echo                                                                                                         ESI 
-echo                                                                                                        \   /
-echo .
-echo                                                                                                  /              \
-echo                                                                                                   Fabian Cabrera
-echo                                                                                                  \              /
-echo .
-echo                                                                                                  CENTRO Y PROFESOR
+echo                                                                                                                         -_INTEGRANTES_-
 echo .
 echo .
-echo                                                                                                  CLASE Y HORARIOS
+echo                                                                                                                      _-CENTRO Y PROFESOR-_
 echo .
-echo                                                                                                    /          \
-echo                                                                                                     Clase 1_BC
-echo                                                                                                    \          /
+echo                                                                                                                              /   \
+echo                                                                                                                               ESI 
+echo                                                                                                                              \   /
 echo .
-echo                                                                                                  /              \
-echo                                                                                                   Turno Matutino
-echo                                                                                                  \              /
+echo                                                                                                                        /              \
+echo                                                                                                                         Fabian Cabrera
+echo                                                                                                                        \              /
 echo .
-echo                                                                                                  CLASE Y HORARIOS
+echo                                                                                                                      -_CENTRO Y PROFESOR_-
 echo .
-echo                                                                                           \----------CREDITOS----------/
-echo                                                                                                    \----------/
-echo                                                                                                     \ PLATON /
+echo .
+echo                                                                                                                       _-CLASE Y HORARIOS-_
+echo .
+echo                                                                                                                           /          \
+echo                                                                                                                            Clase 1_BC
+echo                                                                                                                           \          /
+echo .
+echo                                                                                                                         /              \
+echo                                                                                                                          Turno Matutino
+echo                                                                                                                         \              /
+echo .
+echo                                                                                                                       -_CLASE Y HORARIOS_-
+echo .
+echo                                                                                                                  \----------CREDITOS----------/
+echo                                                                                                                           \----------/
+echo                                                                                                                            \ PLATON /
 pause
 call IDE.bat
